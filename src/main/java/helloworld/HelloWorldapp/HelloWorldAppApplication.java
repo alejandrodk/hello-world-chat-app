b677df94ee6cd2473e25bd7329ea00212c469ce4
Alejandro Barrios <ottoabarriosp@hotmail.com>
2021-03-09 19:43:21 -0300

    Initial commit

---
+++ src/main/java/helloworld/HelloWorldapp/HelloWorldAppApplication.java
@@ -0,0 +1,13 @@
+package helloworld.HelloWorldapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAppApplication.class, args);
+	}
+
+}
